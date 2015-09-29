c81d442391568465e0cd51a7bf18e34f8fdba208
jyh1 <jyh1@mail.ustc.edu.cn>
2015-09-29 08:56:18 +0800

    参数传递引用

--- exercise01/src/zmatrix.cpp
+++ exercise01/src/zmatrix.cpp
@@ -9,7 +9,7 @@ class Zmatrix
 public:
   int id, r3id, r2id,r1id;
   double l, theta, phi;
-  Zmatrix(int idx, int r3idx, double lx, int r2idx, double thetax, int r1idx, double phix):
+  Zmatrix(const int & idx, const int & r3idx, const double & lx, const int & r2idx, const double & thetax, const int & r1idx, const double & phix):
     id(idx), r3id(r3idx), r2id(r2idx), r1id(r1idx), l(lx), theta(thetax), phi(phix) { }
   // Zmatrix(){};
 };
